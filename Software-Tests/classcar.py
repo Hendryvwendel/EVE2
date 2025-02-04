8cd5cdfa0975afa1deece51c43962bb7312249f4
Hendry van Wendel <hendryvwendel@gmail.com>
2025-02-04 08:58:06 +0100

    Get / Set methods added to test.py

--- Software-Tests/classcar.py
+++ Software-Tests/classcar.py
@@ -1,12 +1,26 @@
 class Car:
-    def __init__(self, brand: str, model: str, color: str, power: int, doors: int, apk: bool):
+    def __init__(self, brand: str, model: str, color: str, power: int, doors: int, apk: bool, kenteken: str, kilometerstand: int):
 
         self.brand = brand
         self.model = model
         self.color = color
         self.power = power
         self.doors = doors
-        self.__apk = apk
+        self.apk = apk
+        self.__kilometerstand = kilometerstand
+        self.__kenteken = kenteken
+
+    def get_kenteken(self):
+        return f"Het kenteken van deze auto is {self.__kenteken}"
+    
+    def set_kenteken(self, kenteken: str):
+        self.__kenteken = kenteken
+    
+    def get_kilometerstand(self):
+        return f"De kilometerstand van deze auto is {self.__kilometerstand}"
+    
+    def set_kilometerstand(self, kilometerstand: int):
+        self.__kilometerstand = kilometerstand
 
     def __str__(self):
-        return f"de kleur van {self.brand} {self.model} is {self.color}. Deze auto heeft {self.power} PK en {self.doors} deuren. {'Deze auto mag niet op de weg omdat er geen geldige APK is' if not self.apk else 'Deze auto mag op de weg'}"
+        return f"De kleur van {self.brand} {self.model} is {self.color}. Deze auto heeft {self.power} PK en {self.doors} deuren. {'Deze auto mag niet op de weg omdat er geen geldige APK is' if not self.apk else 'Deze auto mag op de weg'}"
--- Software-Tests/test.py
+++ Software-Tests/test.py
@@ -1,8 +1,7 @@
 from classcar import *
 
-bmw = Car(brand='BMW', model='X5', color='zwart', power=300, doors=5 , apk=False)
+bmw = Car(brand='BMW', model='X5', color='zwart', power=300, doors=5 , apk=False, kenteken=1, kilometerstand=100000)
 
-porsche = Car(brand='Porsche', model='911', color='rood', power=400, doors=2, apk=True)
+porsche = Car(brand='Porsche', model='911', color='rood', power=400, doors=3, apk=True, kenteken=2, kilometerstand=50000)
 
-print(bmw.apk)
-print(porsche)
+
